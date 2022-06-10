7c9c8be2d7affde05020ac2068b4a8741392ed8a
tupatka <patiste18@gmail.com>
2022-06-10 17:52:27 +0200

    usuniecie plikow spoza kontenerow

--- aplikacja/relacje/relacje.mjs
+++ aplikacja/relacje/relacje.mjs
@@ -4,92 +4,88 @@ const amqpUrl = process.env.AMQP_URL || 'amqp://rabbitmq:5672';
 import express from 'express';
 const app = express();
 
+import { sequelize, Match } from './database.mjs';
+
 const port = 5000;
 app.listen(port, () => {
     console.log(`Listening to port http://localhost:${port}`);
 });
 
-const relacje_match = [
-    {
-        id: 1,
-        user_1: 1,
-        user_2: 2,
-    },
-    {
-        id: 2,
-        user_1: 2,
-        user_2: 1,
-    },
-    {
-        id: 3,
-        user_1: 2,
-        user_2: 3,
-    },
-    {
-        id: 4,
-        user_1: 3,
-        user_2: 4,
-    },
-    {
-        id: 5,
-        user_1: 4,
-        user_2: 1
-    },
-    {
-        id: 6,
-        user_1: 1,
-        user_2: 4,
-    },
-]
+const my_id = 1;
 
-app.get('/match/:id', (req, res) => {
+const get_user_1_ids = async (id) => {
+    const map = await Match.findAll(
+        {
+            attributes: [
+                'user_1'
+            ],
+            where: {
+                user_2: id
+            },
+            raw: true
+        }
+    );
+    return map.map(element => element['user_1']);
+}
+
+const get_user_2_ids = async (id) => {
+    const map = await Match.findAll(
+        {
+            attributes: [
+                'user_2'
+            ],
+            where: {
+                user_1: id
+            },
+            raw: true
+        }
+    );
+    return map.map(element => element['user_2']);
+}
+
+// prześlij zbiór matchy dla użytkownika z danym id
+app.get('/match/:id', async (req, res) => {
+    console.log(await Match.findAll({ raw: true }));
+
     const id = parseInt(req.params.id);
     console.log(id);
-    let users_that_we_matched = new Set();
-    let users_that_matched_us = new Set();
-
-    relacje_match.forEach(relacja => {
-        if (relacja.user_1 === id) {
-            users_that_we_matched.add(relacja.user_2);
-        } else if (relacja.user_2 === id) {
-            users_that_matched_us.add(relacja.user_1);
-        }
-    });
+    let users_that_we_matched = await get_user_1_ids(id);
+    let users_that_matched_us = await get_user_2_ids(id);
 
     console.log(users_that_we_matched, users_that_matched_us);
 
-    let intersection = new Set([...users_that_we_matched].filter(x => users_that_matched_us.has(x)));
-    let matches_list = Array.from(intersection);
+    const intersection = users_that_matched_us.filter(value => users_that_we_matched.includes(value));
+    console.log(intersection);
 
-    res.send(matches_list);
+    res.send(intersection);
 })
 
-async function processMessage(msg) {
-    console.log(msg.content.toString(), 'Call email API here');
-    // set last message to msg.content
-}
+// async function processMessage(msg) {
+//     console.log(msg.content.toString(), 'Call email API here');
+//     // set last message to msg.content
+// }
 
-(async () => {
-    const connection = await amqplib.connect(amqpUrl, "heartbeat=60");
-    const channel = await connection.createChannel();
-    channel.prefetch(10);
-    const queue = 'user.add_relation';
-    process.once('SIGINT', async () => {
-        console.log('got sigint, closing connection');
-        await channel.close();
-        await connection.close();
-        process.exit(0);
-    });
+// (async () => {
+//     const connection = await amqplib.connect(amqpUrl, "heartbeat=60");
+//     const channel = await connection.createChannel();
+//     channel.prefetch(10);
+//     const queue = 'user.add_relation';
+//     process.once('SIGINT', async () => {
+//         console.log('got sigint, closing connection');
+//         await channel.close();
+//         await connection.close();
+//         process.exit(0);
+//     });
 
-    await channel.assertQueue(queue, { durable: true });
-    await channel.consume(queue, async (msg) => {
-        console.log('processing messages');
-        await processMessage(msg);
-        await channel.ack(msg);
-    },
-        {
-            noAck: false,
-            consumerTag: 'email_consumer'
-        });
-    console.log(" [*] Waiting for messages. To exit press CTRL+C");
-})();
+//     await channel.assertQueue(queue, { durable: true });
+//     await channel.consume(queue, async (msg) => {
+//         console.log('processing messages');
+//         await processMessage(msg);
+//         await channel.ack(msg);
+//     },
+//         {
+//             noAck: false,
+//             consumerTag: 'email_consumer'
+//         });
+//     console.log(" [*] Waiting for messages. To exit press CTRL+C");
+// })();
